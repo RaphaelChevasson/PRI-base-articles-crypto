fd65330230bf41b9c73f46a6b9374fa86bfb88a8
RaphaelChevasson <rc@clubcapelli.com>
2019-01-29 02:54:40 +0100

    pipeline skeleton working (with the right conda python) on windows

--- launch_pipeline.sh
+++ launch_pipeline.sh
@@ -1,8 +1,39 @@
+env_name="base" # put yours here, default is "base"
+anaconda_path="C:\anaconda3" # only for windows, put yours here
+
+
+
 printf "\n---\n Searching for new publications\n---\n\n"
 java -jar server-0.0.1-SNAPSHOT.jar
+
+
 printf "\n---\n Classifying new publications\n---\n\n"
-#todo: call activate base
+
+# activate python env
+if [[ "$OSTYPE" == "msys" ]]; then # windows
+	# find the _conda_activate command
+	_CONDA_ROOT="$(cygpath $anaconda_path)"
+	\. "$_CONDA_ROOT/etc/profile.d/conda.sh" || return $?
+	# call it
+	_conda_activate "$env_name"
+else                               # linux
+    source activate $env_name
+fi
+
+# run python script
 python test.py
-#todo: call deactivate
+
+# deactivate python env
+if [[ "$OSTYPE" == "msys" ]]; then # windows
+	# find the _conda_deactivate command
+	_CONDA_ROOT="$(cygpath $anaconda_path)"
+	\. "$_CONDA_ROOT/etc/profile.d/conda.sh" || return $?
+	# call it
+	_conda_deactivate
+else                               # linux
+    source deactivate
+fi
+
+
 printf "\n---\n Done!\n---\n\n"
 read -p "Press enter to close..."
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('bla')
+print('this is python')
+
+try:
+	import bibtexparser
+	print('this is conda (unless you have bibtexparser on your default python)')
+except ModuleNotFoundError:
+	print('this is not in conda :/')
